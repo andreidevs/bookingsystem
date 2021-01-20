96f103b3065c532144420d1d975bc3b9dfd82f33
andreidev <andrei.n15@mail.ru>
2021-01-20 23:14:22 +0600

    fix

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 
 import firebase from "firebase/app";
-import firebaseConfig from "./config/firebase2";
+import firebaseConfig from "./config/firebaseMain";
 import "firebase/auth";
 import "firebase/firestore";
 
